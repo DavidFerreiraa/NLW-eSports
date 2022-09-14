edb1db1fb6c08017304d0cddbad6714e1d6704cc
David <dfl8@aluno.ifal.edu.br>
2022-09-14 14:32:46 -0300

    Teste de components and props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,23 @@
 //JSX => JavaScript + XML (HTML)
+//Components and props
+interface ButtonProps {
+  title: String; //Para que a propriedade não seja obrigatória basta colocar uma "?" antes dos ":" de seleção da propriedade
+}
 
+function Button(props: ButtonProps) {
+  return(
+    <button>
+      {props.title}
+    </button>
+  )
+}
 function App() {
   return(
-    <h1>Hello NLW</h1>
+    <div>
+      <Button title="1"/>
+      <Button title="2"/>
+      <Button title="3"/>
+    </div>
   )
 }
 
