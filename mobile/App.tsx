9f5c3ba9b81c99903c5ef154e203f9e907e60a8a
David <dfl8@aluno.ifal.edu.br>
2022-09-14 15:20:22 -0300

    Brincando com React Native e tsc

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello React Native</Text>
+      <Button title='Me toque'/>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+interface ButtonProps {
+  title: String;
+}
+
+function Button(props: ButtonProps) {
+  return(
+    <TouchableOpacity>
+      <Text style={styles.title}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    color: '#fff',
+  }
 });
