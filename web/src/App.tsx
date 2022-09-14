900df2111b3cc886ef05d237c80d1653d1153e99
David <dfl8@aluno.ifal.edu.br>
2022-09-14 16:56:44 -0300

    Adição das imagens no site

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,8 +10,25 @@ function App() {
       <h1 className="text-7xl text-white font-black mt-20">
         Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui.
       </h1>
-      <div className='grid grid-cols-6 gap-6'>
-        
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href='#'>
+          <img src="/image-1.png" alt=""/>
+        </a>
+        <a href='#'>
+          <img src="/image-2.png" alt=""/>
+        </a>
+        <a href='#'>
+          <img src="/image-3.png" alt=""/>
+        </a>
+        <a href='#'>
+          <img src="/image-4.png" alt=""/>
+        </a>
+        <a href='#'>
+          <img src="/image-5.png" alt=""/>
+        </a>
+        <a href='#'>
+          <img src="/image-6.png" alt=""/>
+        </a>
       </div>
     </div>
   )
