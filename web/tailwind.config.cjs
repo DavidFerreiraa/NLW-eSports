02fcba36315df4876475a06617dda7e6c8581e81
David <dfl8@aluno.ifal.edu.br>
2022-09-17 00:11:50 -0300

    Finalização da interface web

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"]
+    },
     extend: {
       backgroundImage: {
         "nlw-gradient": "linear-gradient(89.86deg, #9572FC , #43E7AD, #E1D55D)",
