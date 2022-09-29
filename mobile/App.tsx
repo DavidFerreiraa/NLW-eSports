69dc3d0d2e8131a6440ff4968d3e73f3aecc48d1
David <David>
2022-09-29 14:38:23 -0300

    Criando componente de loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,23 @@
 import { StatusBar } from "react-native";
 import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
 
 export default function App() {
-  useFonts({
+  // fontsLoaded informa se as fontes foram carregadas
+  const [fontsLoaded] = useFonts({
     //Fontes que serão usadas no projeto
     Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black
-  })
+  });
   return (
     <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      />  
+      />
+      { fontsLoaded ? <Home/> : <Loading/> }
     </Background>
   );
 }
