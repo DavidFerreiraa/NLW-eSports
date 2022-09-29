87c61d775e2a7d03836db77d647190d2dc1d9e58
David <David>
2022-09-29 15:09:22 -0300

    Finalização do GameCard

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,11 +1,25 @@
-import { View } from 'react-native';
+import { ImageBackground, ImageSourcePropType, TouchableOpacity, TouchableOpacityProps} from 'react-native';
 
 import { styles } from './styles';
 
-export function GameCard() {
-  return (
-    <View style={styles.container}>
+export interface GameCardProps {
+    id: string,
+    name: string,
+    ads: string,
+    cover: ImageSourcePropType;
+}
 
-    </View>
+interface Props extends TouchableOpacityProps { //Forma de centralizar a tipagem
+    data: GameCardProps;
+}
+
+export function GameCard({data, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+        <ImageBackground
+        style={styles.cover}
+        source={data.cover}
+        />
+    </TouchableOpacity>
   );
 }
