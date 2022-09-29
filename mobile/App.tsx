c50c06b7d974c494e5c85f1ecc6f9b5a0dcbff7a
David <David>
2022-09-29 13:59:52 -0300

    Adição do background

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
 
+Background
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native</Text>
-      <Button title='Me toque'/>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />  
+    </Background>
   );
 }
-
-interface ButtonProps {
-  title: String;
-}
-
-function Button(props: ButtonProps) {
-  return(
-    <TouchableOpacity>
-      <Text style={styles.title}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    color: '#fff',
-  }
-});
