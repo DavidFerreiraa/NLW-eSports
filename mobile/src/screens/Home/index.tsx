c33872e58a1813d571db957ca2a3aef9f7d67fbd
David <David>
2022-09-29 14:39:26 -0300

    Importação da logo

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 
 import { styles } from './styles';
 
+import LogoImg from '../../assets/logo-nlw-esports.png'
+
 export function Home() {
   return (
     <View style={styles.container}>
