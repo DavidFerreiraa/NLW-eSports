1c52cade9b782c83fe5854a1769daa7395446c48
David <David>
2022-10-08 20:22:57 -0300

    Fazendo as requisições à API node retornando-as

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,30 @@ import './styles/main.css'
 import logoImg from './assets/Logo-nlw-esports.svg'
 import GameBanner from './components/GameBanner'
 import CreateAdBanner from './components/CreateAdBanner'
+import { useEffect, useState } from 'react'
 //JSX => JavaScript + XML (HTML)
 //Components and props
 
+interface Games {
+  bannerURL: string;
+  id: string;
+  title: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+  const [games, setGames] = useState<Games[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+    .then(response => response.json())
+    .then(data => {
+        setGames(data)
+      })
+  }, [])
+
   return(
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt=""></img>
@@ -13,7 +33,17 @@ function App() {
         Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui.
       </h1>
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl="../../public/image-1.png" title="League of legends" adsCount={3}/>
+        {games.map(game => {
+          console.log(game)
+          return(
+            <GameBanner 
+            key={game.id}
+            bannerUrl={game.bannerURL} 
+            title={game.title} 
+            adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
       <CreateAdBanner/>
     </div>
--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -8,7 +8,7 @@ interface GameBannerProps {
 export default function GameBanner(props: GameBannerProps){
     return(
         <a href='#' className="relative rounded-lg overflow-hidden">
-          <img src={props.bannerUrl} alt=""/>
+          <img src={props.bannerUrl} alt="" className="object-cover"/>
           <div className="w-full pt-16 pb-4 px-4 bg-imgs-gradient absolute bottom-0 left-0 right-0">
             <strong className="font-bold text-white block">{props.title}</strong>
             <span className="text-zinc-300 text-sm block">{props.adsCount} {props.adsCount === 1? 'anúncio': 'anúncios'}</span>
