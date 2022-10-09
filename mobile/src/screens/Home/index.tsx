1fe295cce3bc96fc30b80f01940877437fe1216d
David <David>
2022-10-09 13:39:22 -0300

    Consulta da API node

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
 
 import { styles } from './styles';
 
@@ -8,8 +9,24 @@ import { GameCard } from '../../components/GameCard';
 
 import { GAMES } from '../../utils/games';
 
-export function Home() {
+interface Games {
+  bannerURL: string;
+  id: string;
+  title: string;
+  _count: {
+    ads: number
+  }
+}
 
+export function Home() {
+  const [games, setGames] = useState<Games[]>([])
+  useEffect(() => {
+    fetch('http://192.168.1.111:3333/games')
+    .then((response) => response.json())
+    .then(data => {
+      setGames(data)
+    })
+  }, []);
   return (
     <View style={styles.container}>
         <Image 
@@ -21,7 +38,7 @@ export function Home() {
         subtitle='Selecione o game que quer jogar'
         />
         <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard
